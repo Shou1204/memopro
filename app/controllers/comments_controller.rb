e639601bd005d014f0e04d56dbe97a5f1579ba8b
Shou1204 <flyingsheep0000@gmail.com>
2023-01-26 21:51:20 +0900

    コメントの投稿、削除にフラッシュメッセージを追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,12 +4,14 @@ class CommentsController < ApplicationController
     @comment = Comment.new(comment_params)
     @memo = Memo.find(params[:memo_id])
     Comment.create(comment_params)
+    flash[:notice] = 'コメントを投稿しました'
     redirect_to memo_path(params[:memo_id])
   end
 
   def destroy
     comment = Comment.find(params[:id])
     @comment = comment.destroy
+    flash[:notice] = 'コメントを削除しました'
     redirect_to memo_path(params[:memo_id])
   end
   private
