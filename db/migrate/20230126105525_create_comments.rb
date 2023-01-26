f1111d8fe4725008b256dd0306f5720ded3e3104
Shou1204 <flyingsheep0000@gmail.com>
2023-01-26 19:58:54 +0900

    commentsテーブル作成

--- db/migrate/20230126105525_create_comments.rb
+++ db/migrate/20230126105525_create_comments.rb
@@ -1,7 +1,9 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-
+      t.string :text,     null: false
+      t.references :memo, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
