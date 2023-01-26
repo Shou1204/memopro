0db5fef5f8588bfbf89398886b724485535bb6b2
Shou1204 <flyingsheep0000@gmail.com>
2023-01-26 14:57:18 +0900

    投稿画面の見た目設定

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,4 +1,23 @@
 class MemosController < ApplicationController
   def index
   end
+
+  def new
+    @memo = Memo.new
+  end
+
+  def create
+    @memo = Memo.new(memo_params)
+    if @memo.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def memo_params
+    params.require(:memo).permit(:title, :text, :tag, :image)
+  end
 end
