10cb405f4adda2587d1ecdf4a2f8d9cbc2dfb0bf
Shou1204 <flyingsheep0000@gmail.com>
2023-01-26 16:17:49 +0900

    投稿機能に画像を追加

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -1,2 +1,9 @@
 class Memo < ApplicationRecord
+  has_one_attached :image 
+
+  validates :content, presence: true, unless: :was_attached?
+
+  def was_attached?
+    self.image.attached?
+  end
 end
