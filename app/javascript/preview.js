7380fae000672941e3fb0fa6a9294df1df560bb3
Shou1204 <flyingsheep0000@gmail.com>
2023-01-29 22:17:26 +0900

    投稿画面のプレビューをマークダウン対応に

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,8 +1,14 @@
-window.addEventListener('load', () => {
-  const previewInput = document.getElementById("memo-code");
-  previewInput.addEventListener("input", () => {
-    const previewValue = previewInput.value;
-    const memoMarkDown = document.getElementById("preview");
-    memoMarkDown.innerHTML = (previewValue)
- })
-});
+window.onload = function() {
+  new Vue({
+    el: '#editor',// index.htmlでid="app"となっている要素（エレメント）を指定
+    data: {
+        input: ''// index.htmlでv-model="input"が付与されている要素と双方向データバインディングされている。
+    },
+    computed: {
+  convertMarkdown: function() {
+// index.htmlでv-html="convertMarkdown"が付与されている要素（エレメント）とバイディングされている。
+// 入力されたデータをHTMLに変換して表示させる。
+return marked(this.input);
+}
+}
+})};
