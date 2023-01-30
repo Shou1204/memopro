c3c6d6d91b8a32e98424406cf9869541278fda73
Shou1204 <flyingsheep0000@gmail.com>
2023-01-30 16:11:41 +0900

    マークダウンプレビューの仕様変更

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,14 +1,18 @@
-window.onload = function() {
-  new Vue({
-    el: '#editor',// index.htmlでid="app"となっている要素（エレメント）を指定
-    data: {
-        input: ''// index.htmlでv-model="input"が付与されている要素と双方向データバインディングされている。
-    },
-    computed: {
-  convertMarkdown: function() {
-// index.htmlでv-html="convertMarkdown"が付与されている要素（エレメント）とバイディングされている。
-// 入力されたデータをHTMLに変換して表示させる。
-return marked(this.input);
-}
-}
-})};
+function markdownCode() {
+  const codeInput = document.getElementById('input_code');  // idが'user_answer_code'のもの(textarea)
+  const codeMarkdown = document.getElementById('code_markdown');  // idが'code_markdown'のもの(空のdiv)
+  if (codeInput == null || codeMarkdown == null) {
+    return;  // 該当ページでない場合はアーリーリターン
+  }
+
+  codeInput.addEventListener('input', () => {
+    const HTML = `${codeInput.value}`;
+    codeMarkdown.innerHTML = marked(HTML);
+    const pre_code_nodes = document.querySelectorAll("pre code");  // ここがポイント
+    for(let i = 0; i < pre_code_nodes.length; ++i){                // ここがポイント
+      hljs.highlightBlock(pre_code_nodes[i]);                      // ここがポイント
+    }
+  });
+};
+
+window.addEventListener("load", markdownCode);
