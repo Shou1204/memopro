45ffe4a868bf415bd18012bf45e6289f7473d481
Shou1204 <flyingsheep0000@gmail.com>
2023-02-07 18:52:38 +0900

    アバターのicon設定と画像の追加

--- app/models/avatar.rb
+++ app/models/avatar.rb
@@ -2,10 +2,10 @@ class Avatar < ActiveHash::Base
   self.data = [
     { id: 1, name: '---', icon: ''},
     { id: 2, name: '勇者', icon: 'ch_yusha.png'},
-    { id: 3, name: '戦士', icon: ''},
-    { id: 4, name: '魔法使い', icon: ''},
-    { id: 5, name: '僧侶', icon: ''},
-    { id: 6, name: '村人', icon: ''}
+    { id: 3, name: '戦士', icon: 'ch_senshi.png'},
+    { id: 4, name: '魔法使い', icon: 'ch_mahou.png'},
+    { id: 5, name: '僧侶', icon: 'ch_souryo.png'},
+    { id: 6, name: '村人', icon: 'ch_murabito.png'}
   ]
 
   include ActiveHash::Associations
