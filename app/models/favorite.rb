24ad578944e17ec39a3a261f8f4c63ad81a5fc7e
Shou1204 <flyingsheep0000@gmail.com>
2023-02-19 13:03:27 +0900

    バリデーションの記述

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,7 @@
 class Favorite < ApplicationRecord
   belongs_to :memo
   belongs_to :user
+
+  validates :memo_id, uniqueness: { scope: :user_id, message: "is already favorited by this user" }
+
 end
