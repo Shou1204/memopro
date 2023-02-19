a13f568fa37ebb36df04034e79f08070b403e14e
Shou1204 <flyingsheep0000@gmail.com>
2023-02-19 19:08:12 +0900

    favoriteのcreateアクションの設定

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,8 +1,17 @@
 class FavoritesController < ApplicationController
   def create
+    @favorite = Favorite.new(favorite_params)
+    @memo = Memo.find(params[:memo_id])
+    Favorite.create(favorite_params)
   end
 
   def destroy
   end
-  
+
+  private
+
+  def favorite_params
+    params.permit(:memo_id).merge(user_id: current_user.id)
+  end
+
 end
