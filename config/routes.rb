468fa866ed7af8d88ac1f7e3b68e5d25577e6aa5
Shou1204 <flyingsheep0000@gmail.com>
2023-02-19 13:05:34 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "memos#index"
   resources :memos do
-    resources :comments, only:[:create, :update, :destroy,]
+    resources :comments, only:[:create, :update, :destroy]
   end
   resources :users, only:[:show]
+  resources :favorites, only:[:create, :destroy]
 end
